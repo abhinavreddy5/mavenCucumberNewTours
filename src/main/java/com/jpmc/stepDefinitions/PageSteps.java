5584693850b724d88f36c87878679cc55daf1a29
abhinavreddy5 <abhinavreddy5@gmail.com>
2017-07-02 20:34:20 +1000

    Update PageSteps.java

--- src/main/java/com/jpmc/stepDefinitions/PageSteps.java
+++ src/main/java/com/jpmc/stepDefinitions/PageSteps.java
@@ -42,7 +42,7 @@ public class PageSteps extends PageStepsObjects {
 		
 		gp.search().sendKeys(text);
 		System.out.println("Entered text = "+text);
-		
+		System.out.println("Hey There GIT");
 	}
 	
 	@Then("^I close the browser$")
